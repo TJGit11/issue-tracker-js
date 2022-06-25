1634e56f8f74b44981fe504f7c7eebf3a05eaa57
TJ <thomasjjohnson9311@gmail.com>
2022-06-25 12:13:43 -0500

    save & delete issue, closed status

--- main.js
+++ main.js
@@ -36,5 +36,52 @@ function saveIssue(e) {
     var issue = {
         id: issueId,
         description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
     }
-}
+
+    if (localStorage.getItem('issues') === null) {
+        var issues = [];
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+        var issues = JSON.parse(localStorage.getItem('issues'));
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    }
+    document.getElementById('issueInputForm').requestFullscreen();
+
+    fetchIssues();
+
+    e.preventDefault();
+
+    }
+
+    function setStatusClosed (id) {
+        var issues = JSON.parse(localStorage.getItem('issues'));
+
+        for(var i = 0; i < issues.legnth; i++) {
+            if (issues[i].id == id) {
+                issues[i].status = "Closed";
+            }
+        }
+
+        localStorage.setItem('issues', JSON.stringify(issues));
+
+        fetchIssues();
+    }
+
+    function deleteIssue (id) {
+        var issues = JSON.parse(localStorage.getItem('issues'));
+
+        for (var i = 0; i < issues.length; i++) {
+            if (issues[i].id == id) {
+                issues.splice(i, 1);
+            }
+        }
+
+        localStorage.setItem('issues', JSON.stringify(issues));
+
+        fetchIssues();
+    }
